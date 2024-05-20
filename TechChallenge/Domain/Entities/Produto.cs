43066d30898cfe1b5c9c168d5bef6941ef2d4b60
Matheus Luz <matheus.luz@credisis.com.br>
2024-05-20 18:54:12 -0300

    Adiciona comportamento de atualizar dados na entidade produto

--- TechChallenge/Domain/Entities/Produto.cs
+++ TechChallenge/Domain/Entities/Produto.cs
@@ -17,10 +17,34 @@ namespace Domain.Entities
             Preco = preco;
         }
 
+        public Produto(string nome, string descricao, CategoriaProduto categoria, decimal preco)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Categoria = categoria;
+            Preco = preco;
+        }
+
         public int Id { get; private set; }
         public string Nome { get; private set; } = string.Empty;
         public string Descricao { get; private set; } = string.Empty;
         public CategoriaProduto Categoria { get; private set; }
         public decimal Preco { get; private set; }
+
+        public void Atualiza(string? nome, string? descricao, CategoriaProduto? categoria, decimal? preco)
+        {
+            this.Nome = nome ?? this.Nome;
+            this.Descricao = descricao ?? this.Descricao;
+
+            if (categoria != null && categoria != this.Categoria)
+            {
+                this.Categoria = (CategoriaProduto)categoria;
+            }
+
+            if (preco != null && preco != this.Preco)
+            {
+                this.Preco = (decimal)preco;
+            }
+        }
     }
 }
